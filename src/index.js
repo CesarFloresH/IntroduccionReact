631e513723c15657134a289395754718518c014d
CesarFloresH <cesar25.566@gmail.com>
2022-05-03 17:52:25 -0600

    Se movió el método handleClick de la clase Board a la clase Game además se actualizo este método y la versión del package.json.

--- src/index.js
+++ src/index.js
@@ -17,15 +17,6 @@ function Square(props) {
       };
     }
 
-    handleClick(i) {
-      const squares = this.state.squares.slice();
-      if (calculateWinner(squares) || squares[i]) {
-        return;
-      }
-      squares[i] = this.state.xIsNext ? 'X': 'O';
-      this.setState({squares: squares, xIsNext: !this.state.xIsNext,});
-    }
-
     renderSquare(i) {
       return (
         <Square 
@@ -67,6 +58,21 @@ function Square(props) {
         xIsNext: true,
       };
     }
+
+    handleClick(i) {
+      const history = this.state.history;
+      const current = history[history.length - 1];
+      const squares = current.squares.slice();
+      if (calculateWinner(squares) || squares[i]) {
+        return;
+      }
+      squares[i] = this.state.xIsNext ? 'X': 'O';
+      this.setState({
+        history: history.concat([{
+          squares: squares,
+        }]), xIsNext: !this.state.xIsNext,});
+    }
+    
     render() {
       const history = this.state.history;
       const current = history[history.length - 1];
